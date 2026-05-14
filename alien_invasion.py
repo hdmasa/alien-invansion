fc063a0d3a6837ba0a2e07cd4128266aed854d60
hdmasa <mahsasamie894@gmail.com>
2026-05-14 03:30:55 +0330

    added the ship modul but the color of the ship image doesn't match

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,37 +1,44 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
-  """Overall class to manage game assets and behavior"""
+    """Overall class to manage game assets and behavior."""
 
-  def __init__(self):
-    """Initilize the game , and create game resourses."""
-    pygame.init()
+    def __init__(self):
+        """Initialize the game and create game resources."""
+        pygame.init()
 
-    self.settings = Settings()
+        self.settings = Settings()
 
-    self.screen = pygame.display.set_mode(
-      (self.settings.screen_width, self.settins.screen_height))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,
+             self.settings.screen_height)
+        )
 
-    pygame.display.set_caption("Alien Invasion")
+        pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
 
     def run_game(self):
-      """Start the main loop for the game"""
-      while True:
-        # Watch for keyboard and mout events
-        for even in pygame.event.get():
-          if even.type == pygame.QUIT:
-            sys.exit()
+        """Start the main loop for the game."""
+        while True:
 
-            #redraw the screen during each pass through the loop
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
-          
-          #Make the most recently drawn screen visible
-          pygame.display.filp()
+            self.ship.blitme()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
 
 if __name__ == '__main__':
-  #Make a game instace, and run the game.
-  ai = AlienInvasion()
-  ai.run_game()
+    ai = AlienInvasion()
+    ai.run_game()
